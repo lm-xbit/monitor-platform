4c6f0148b475d337709244484b322810abb8a812
qiao dan <dan@CTU-DQIAO.local>
2016-07-19 14:10:55 +0800

    Add web site

--- xbit/routes/users.js
+++ xbit/routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('login', {
+    title: 'xBit Login'
+  });
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
