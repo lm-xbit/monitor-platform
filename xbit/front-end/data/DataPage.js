ca9b26685af19fc4a18d50a4cf5c05dbaac155b9
qiao dan <dan@CTU-DQIAO.local>
2016-08-04 11:14:42 +0800

    dan change data page

--- xbit/front-end/data/DataPage.js
+++ xbit/front-end/data/DataPage.js
@@ -2,12 +2,40 @@ import React from 'react';
 
 export default function GraphPage (props) {
   return (
-    <h4>
-      This is Data page!
-    </h4>
+    <div>
+      <button type="button" class="btn btn-primary">立即刷新</button>
+      <table class="table table-striped">
+    <thead>
+        <tr>
+          <th>时间</th>
+          <th>经度</th>
+          <th>纬度</th>
+          <th>速度</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">2016-08-01 20:01:12 PDT</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2016-08-02 20:01:12 PDT</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">2016-08-03 20:01:12 PDT</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
   );
 };
 
-GraphPage.propTypes = {
-};
-
+GraphPage.propTypes = {};
