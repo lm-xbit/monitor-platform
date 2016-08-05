4471a5354bc652e19f9250f395177f5a2ae385fd
inkie.wu <inkie.wu@logicmonitor.com>
2016-08-05 13:18:41 +0800

    fix map issue

--- xbit/front-end/graph/GraphPage.js
+++ xbit/front-end/graph/GraphPage.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-export default function GraphPage (props) {
-  return (
-    <div>
-      <div style={{ margin: 'auto', width: '1000px', height: '600px' }} id="map-container" >
-      </div>
+export class GraphPage extends React.Component {
+  componentDidMount () {
+    var map = new window.AMap.Map('map-container');
+  }
+
+  render () {
+    return (
       <div>
-        {
-          new AMap.Map('map-container')
-        }
+        <div style={{ margin: 'auto', width: '1000px', height: '600px' }} id="map-container" >
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 GraphPage.propTypes = {
 };
+
+export default GraphPage;
