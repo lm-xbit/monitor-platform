c1024ea26220a2247bcdc6b74c9d536221017e6d
raye <lingzhi.@163.com>
2016-08-08 16:02:05 +0800

    add register and login related api without authentication

--- xbit/routes/index.js
+++ xbit/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('models/user');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('signup', {
@@ -18,18 +20,6 @@ router.get('/login', function (req, res) {
   res.render('login');
 });
 
-router.post('/rest/settings/register', function (req, res) {
-  Account.register(new Account( { username : req.body.username }), req.body.password, function (err, account) {
-    if (err) {
-      //return res.render('register', { account : account });
-      return res.send("Fail to register the user - " + err);
-    }
-
-    res.send("Success to register the user");
-  })
-});
-
-//////
 router.use("/api", require("./api"));
 
 router.use("/rest", require("./rest"));
--- xbit/routes/rest/settings.js
+++ xbit/routes/rest/settings.js
@@ -1,24 +1,108 @@
 var express = require('express');
 var passport = require('passport');
 var Account = require('models/account');
+var User = require('models/user');
+
+var Resp = require('resp')
+var StatusCodes = require('status')
 
 var router = express.Router();
 
-router.post("/login", passport.authenticate('basic'), function (req, res) {
-  res.send("Success to login");
+
+router.post('/register', (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.where({username: username}).findOne( (err, user) => {
+    if (err) {
+      return res.json(new Resp(StatusCodes.INTERNAL_ERROR, "register failed for interal error"));
+    }
+
+    if (user) {
+      return res.json(new Resp(StatusCodes.USER_EXISTS, "username has existed, please choose another username"));
+    }
+
+    // save new user to db
+    user = new User({username: username, password: password});
+    user.save( (err, user, numAffected) => {
+      if (err || numAffected == 0) {
+        return res.json(new Resp(StatusCodes.INTERNAL_ERROR, "save user failed"));
+      }
+
+      return res.json(new Resp(StatusCodes.OK, "registry successfully", user.toJSON()));
+    });
+  });
 });
 
-router.get("/users", passport.authenticate('basic'), function (req, res) {
+router.post('/login', (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
 
+  User.where( {username: username, password: password} ).findOne( (err, user) => {
+    if (err || !user) {
+      return res.json(new Resp(StatusCodes.LOGIN_FAILED, "invalid username or password"));
+    }
+    if (user) {
+      // todo: update session
 
-    Account.find({}, function (err, doc) {
-      if (err) {
-        return res.send("Fail to list all users");
+      return res.json(new Resp(StatusCodes.OK, StatusCodes.OK_STR, user.toJSON()));
+    }
+  });
+});
+
+router.get('/user/:id', (req, res) => {
+  var uid = req.params.id;
+
+  if (!uid) {
+    return res.json(new Resp(StatusCodes.BAD_REQUEST, "missing user id"));
+  }
+
+  User.where( {_id: uid} ).findOne( (err, user) => {
+    if (err || !user) {
+      return res.json(new Resp(StatusCodes.NOT_FOUND, "user not found"));
+    }
+
+    if (user) {
+      return res.json(new Resp(StatusCodes.OK, StatusCodes.OK_STR, user.toJSON()));
+    }
+  });
+});
+
+router.put('/user/:id', (req, res) => {
+  var uid = req.params.id;
+  var user = req.body;
+
+  if (!uid || !user) {
+    return res.json(new Resp(StatusCodes.BAD_REQUEST, "missing uid or user data"));
+  }
+
+  user.id = uid;
+  if (user.userKeys && user.userKeys instanceof Array && user.userKeys.length > 0) {
+    // user.userKeys = [{key: xxx, updateOn: xxx, createOn: xxx}]
+    user.userKeys.forEach( (value, index, array) => {
+      if (!value || !(value instanceof Object)) {
+        array.splice(index, 1);
+        return;
       }
-      res.json(doc);
-    });
 
-  //res.send("Success to get users");
+      if (!value.key) {
+        array.splice(index, 1); // remove this element from array
+        return;
+      }
+    });
+  }
+  var update = {}
+  update.userKeys = user.userKeys;
+  if (user.password) {
+    update.password = user.password;
+  }
+
+  User.findByIdAndUpdate(uid, {$set: update}, {new: true, upsert: true}, (err, user) => {
+    if (err || !user) {
+      return res.json(new Resp(StatusCodes.INTERNAL_ERROR, "update user info failed"));
+    }
+    return res.json(new Resp(StatusCodes.OK, StatusCodes.OK_STR, user.toJSON()));
+  });
 });
 
 module.exports = router;
