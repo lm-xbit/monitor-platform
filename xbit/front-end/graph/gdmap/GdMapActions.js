1ee913d8f627f5fa423d0bfb496bd8a1bef340c6
Ning <ning.chen@logicmonitor.com>
2016-08-08 17:24:10 +0800

    convert GPS locations

--- xbit/front-end/graph/gdmap/GdMapActions.js
+++ xbit/front-end/graph/gdmap/GdMapActions.js
@@ -1,10 +1,13 @@
 import $ from 'jquery';
+import converter from 'coordtransform';
 
 export const getLocation = (map) => {
   $.getJSON('/rest/data/mobile-tracking', '', function (json) {
     map.setZoom(14);
     var latestData = json.data[json.data.length - 1];
-    map.setCenter([latestData.location.longitude, latestData.location.latitude]);
+    var converted = converter.wgs84togcj02(latestData.location.longitude, latestData.location.latitude);
+
+    map.setCenter(converted);
     var marker = new window.AMap.Marker({
       position: [latestData.location.longitude, latestData.location.latitude],
       map: map
