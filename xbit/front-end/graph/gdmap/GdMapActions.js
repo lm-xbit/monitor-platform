315c14ccfe41c14a4bcb8cac78deb6ef3ebf5d3c
Ning <ning.chen@logicmonitor.com>
2016-08-08 22:26:17 +0800

    GPS position offset

--- xbit/front-end/graph/gdmap/GdMapActions.js
+++ xbit/front-end/graph/gdmap/GdMapActions.js
@@ -1,14 +1,20 @@
 import $ from 'jquery';
+import converter from 'coordtransform';
 
 export const getLocation = (map) => {
   $.getJSON('/rest/data/mobile-tracking', '', function (json) {
     map.setZoom(14);
-    var latestData = json.data[json.data.length - 1];
-    map.setCenter([latestData.location.longitude, latestData.location.latitude]);
-    var marker = new window.AMap.Marker({
-      position: [latestData.location.longitude, latestData.location.latitude],
-      map: map
-    });
+    if(json.data && json.data.length > 0) {
+      var latestData = json.data[json.data.length - 1];
+      var converted = converter.wgs84togcj02(latestData.location.longitude, latestData.location.latitude);
+
+      map.setCenter(converted);
+      var marker = new window.AMap.Marker({
+        // position: [latestData.location.longitude, latestData.location.latitude],
+        position: [converted[0], converted[1]],
+        map: map
+      });
+    }
   });
 };
 
