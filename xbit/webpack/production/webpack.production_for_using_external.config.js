888730f2496f7ad22869bcc4e96352b24b604a37
Ning <ning.chen@logicmonitor.com>
2016-09-02 09:26:21 +0800

    fix building

--- xbit/webpack/production/webpack.production_for_using_external.config.js
+++ xbit/webpack/production/webpack.production_for_using_external.config.js
@@ -22,12 +22,13 @@ config.plugins = config.plugins.concat([
     compress: {
       warnings: false
     }
-  }),
+  })
 
+/*
   new ExtractTextPlugin(`${publicPath}/styles/[name].css?[contenthash]`, {
     allChunks: true
   })
-  /*
+
   new HtmlWebpackPlugin({
     filename: 'index.html',
     template: 'front-end/resources/index.html'
@@ -37,21 +38,33 @@ config.plugins = config.plugins.concat([
 
 config.module.loaders = config.module.loaders.concat(
     {
-      // inline base64 URLs for <=8k images, direct URLs for the rest
-      test: /\.(png|woff|woff2|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=8192'
-    },
-    {
       test: /\.png|gif/, loader: `url-loader?limit=1000&name=${publicPath}/images/[sha512:hash:base64:7].[ext]`
     },
     {
       test: /\.jpg/, loader: `file-loader?name=${publicPath}/images/[sha512:hash:base64:7].[ext]`
     },
     {
+        // test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader'
+        test: /\.scss$/, loader: 'style!css?modules!sass'
+    },
+    {
+        test: /\.less$/, loader: 'style-loader!css-loader!less-loader' // use ! to chain loaders
+    },
+    {
+        test: /\.css$/, loader: 'style-loader!css-loader'
+    },
+    {
+        // inline base64 URLs for <=8k images, direct URLs for the rest
+        test: /\.(png|woff|woff2|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=8192'
+    }
+    /*
+    {
       test: /\.s?css$/,
       loader: ExtractTextPlugin.extract('style-loader', specificConfig.useCssModules
           ? 'css-loader?modules&importLoaders=1&localIdentName=__[local]__[hash:base64:5]!postcss-loader'
           : 'css-loader?sourceMap!postcss-loader!sass-loader', {publicPath: '../../'})
     }
+    */
 );
 
 delete config.output.publicPath;
