5452f66b75229152450a7979f49654f9391f3b73
inkie.wu <inkie.wu@logicmonitor.com>
2016-09-05 20:15:52 +0800

    delete config file

--- xbit/config.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = {
-    ssl: false,
-    port: 8080,
-    dns: "www.xbit.ga",
-    address: "127.0.0.1",
-    sesAWSId: "xxxx",
-    sesAWSKey: "xxxx",
-    sesRegion: "us-west-2"
-};
