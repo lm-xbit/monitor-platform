9d7243e619bee88d07f6e8be93449091e8c8b88b
Ning <ning.chen@logicmonitor.com>
2016-09-14 10:39:23 +0800

    permission stuff

---
+++ agent-android/gpslogger/src/main/java/com/mendhak/gpslogger/utils/PermissionUtil.java
@@ -0,0 +1,29 @@
+package com.mendhak.gpslogger.utils;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by ning.chen on 9/14/16.
+ */
+public class PermissionUtil {
+    /**
+     * Check if the permissions passed as the second and forthward arguments are granted or not
+     * @param context
+     * @param permissions
+     * @return
+     */
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
