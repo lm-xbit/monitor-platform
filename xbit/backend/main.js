95f8122090595473b86644d4849abef28003e54e
robin.deng <robin.deng@logicmonitor.com>
2016-11-13 23:56:28 +0800

    Add redis to store the offset

--- xbit/backend/main.js
+++ xbit/backend/main.js
@@ -8,16 +8,20 @@
 var kafka = require('kafka-node') ;
 var ESClient = require("lib/esclient");
 var xBitLogger = require("xBitLogger");
+var redis = require("lib/redis");
 
 var logger = xBitLogger.createLogger({module: 'main'});
+
+topic = "data-mobile-tracking";  // Let's make topic as "data-" + <appName>
 
 class Server {
     constructor() {
         this.kafkaClient = null;
         this.consumer = null;
-        this.offset = null;
+        this.kafkaOffset = null;
         this.topics = [];
     }
+
 
     /**
      * Start the server
@@ -26,33 +30,48 @@ class Server {
         // 1. offset manager to recover offset errors
         logger.info("Create KAFKA client with connection string %s", config.connectString);
         this.kafkaClient = new kafka.Client(config.connectString || '127.0.0.1:2181/');
-        this.offset = new kafka.Offset(this.kafkaClient);
+        this.kafkaOffset = new kafka.Offset(this.kafkaClient);
+        var server = this;
+        redis.get(topic, function (err, value) {
+           if (err) {
+               logger.error("Fail to get the offset from redis - " + err);
+           }
+           else {
+               var offset = 0;
+               if (value) {
+                   logger.info("Get the offset - " + value);
+                   offset = parseInt(value);
+               }
+               //2. create topics
+               server.topics = [{
+                   topic : topic,
+                   offset: offset                       // TODO recover offset from MongoDB or Redis
+               }];
 
-        //2. create topics
-        this.topics = [{
-            topic: "data-mobile-tracking",  // Let's make topic as "data-" + <appName>
-            offset: 0                       // TODO recover offset from MongoDB or Redis
-        }];
+               logger.info("Loaded KAFKA %d topics", server.topics.length);
+               server.topics.forEach(function(topic) {
+                   logger.info("Found topic %s with offset %d", topic.topic, topic.offset);
+               });
 
-        logger.info("Loadded KAFKA %d topics", this.topics.length);
-        this.topics.forEach(function(topic) {
-            logger.info("Found topic %s with offset %d", topic.topic, topic.offset);
+               // 3. create the consumer
+               server.consumer = new kafka.Consumer(server.kafkaClient, server.topics, {
+                   groupId: config.groupId,
+                   autoCommit: false,
+                   fetchMaxWaitMs: 1000,
+                   fetchMaxBytes: 1024 * 1024,
+                   fromOffset: true
+               });
+
+               logger.info("Created KAFKA consumers with groupId %s", config.groupId);
+
+               // 4. roll the consumer by setting up the listeners
+               server.consumer.on("message", server._handleMessage.bind(this));
+               server.consumer.on("error", server._handleError.bind(this));
+               server.consumer.on("offsetOutOfRange", server._handleOffsetOutOfRange.bind(this));
+
+           }
         });
 
-        // 3. create the consumer
-        this.consumer = new kafka.Consumer(this.kafkaClient, this.topics, {
-            groupId: config.groupId,
-            autoCommit: false,
-            fetchMaxWaitMs: 1000,
-            fetchMaxBytes: 1024 * 1024
-        });
-
-        logger.info("Created KAFKA consumers with groupId %s", config.groupId);
-
-        // 4. roll the consumer by setting up the listeners
-        this.consumer.on("message", this._handleMessage.bind(this));
-        this.consumer.on("error", this._handleError.bind(this));
-        this.consumer.on("offsetOutOfRange", this._handleOffsetOutOfRange.bind(this));
     }
 
     _handleMessage(message) {
@@ -75,13 +94,29 @@ class Server {
          * }
          */
         let data = eval("(" + message.value + ")");
-        let deviceKey = data.key;
-        let that = this;
-
         let timestamp = data['@timestamp'];
 
         logger.info("Indexing sample with timestamp %d: %s", timestamp, message.value);
-        that._indexData(timestamp, deviceKey, data.location);
+
+        var doc = {
+            key: data.key,
+            '@timestamp': timestamp,
+            'location': data.location
+        };
+        // TODO compose the doc according to the ES mapping
+        ESClient.index({
+            index: "xbit",
+            type: "geoData",
+            body: doc
+        });
+        redis.incr(topic, function (err, response) {
+            if (err) {
+                logger.error("Fail to set the topic %s offset to redis", topic);
+            }
+            else {
+                logger.info("Set the offset of topic %s to redis", topic);
+            }
+        })
     }
 
     _handleError(err) {
@@ -92,7 +127,7 @@ class Server {
         topic.maxNum = 2;
 
         let that = this;
-        this.offset.fetch([topic], function (err, offsets) {
+        this.kafkaOffset.fetch([topic], function (err, offsets) {
             if (err) {
                 return console.error(err);
             }
@@ -103,20 +138,20 @@ class Server {
         })
     }
 
-    _indexData(timestamp, key, location) {
-        var doc = {
-            key: key,
-            '@timestamp': timestamp,
-            'location': location
-        };
-
-        // TODO compose the doc according to the ES mapping
-        return ESClient.index({
-            index: "xbit",
-            type: "geoData",
-            body: doc
-        });
-    }
+    // _indexData(timestamp, key, location) {
+    //     var doc = {
+    //         key: key,
+    //         '@timestamp': timestamp,
+    //         'location': location
+    //     };
+    //
+    //     // TODO compose the doc according to the ES mapping
+    //     return ESClient.index({
+    //         index: "xbit",
+    //         type: "geoData",
+    //         body: doc
+    //     });
+    // }
 }
 
 exports = module.exports = new Server();
