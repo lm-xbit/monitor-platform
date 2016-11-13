0de7ec7e6870d41fd8a9712032436b0618ea0964
robin.deng <robin.deng@logicmonitor.com>
2016-11-13 22:34:08 +0800

    Update backend to pull data from kafka

--- xbit/config.template.js
+++ xbit/config.template.js
@@ -11,6 +11,7 @@ exports = module.exports = {
     sesAWSKey: "xxxxxxx",
     sesRegion: "us-west-2",
     logFilePath: "xbitLog",
+    connectString: "zookeeper:2181",
     kafka: {
         address: "127.0.0.1",
         port: "2181"
